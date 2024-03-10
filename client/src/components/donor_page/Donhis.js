f070716c452e99fec8423897b618b93936c74eac
darshani2 <sewwandidarshani95@gmail.com>
2024-03-10 08:36:39 -0700

    Add Donor API integration

--- client/src/components/donor_page/Donhis.js
+++ client/src/components/donor_page/Donhis.js
@@ -1,15 +1,33 @@
 import axios from 'axios';
-import React, { useEffect, useState } from "react";
+import React, { useLocation, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import '../../App.css';
 
 function Donhis() {
+  const { state } = useLocation();
   const [users, setUsers] = useState([])
     
+  
     useEffect(()=>{
+      /*
     axios.get('http://localhost:8081/user/makedonation')
     .then(result => setUsers(result.data))
     .catch(err => console.log(err))
+    */
+   axios
+      .get('http://localhost:8081/user/donor/get', state.data)
+      .then((res) => {
+        if (res.status === 200) {
+      console.log(res)
+        } else {
+          alert('Donation was unsuccessful. Check your credentials.');
+        }
+      })
+      .catch((err) => {
+        console.error('Error while fetching user data:', err);
+        alert('An error occurred while fetching user data.');
+      });
+   
   })
 
   return (
--- client/src/components/donor_page/Makedon.js
+++ client/src/components/donor_page/Makedon.js
@@ -1,63 +1,153 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 function Makedon() {
-    const[name,setName] = useState()
-    const[email,setEmail] = useState()
-    const[phone,setPhone] = useState()
-    const[item,setSize] = useState()
-    const[quantity,setQuantity] = useState()
-    const[location,setLocation] = useState()
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    item: '',
+    quantity: '',
+    location: '',
+  });
 
-    const Donate = (e) =>{
-        e.preventDefault();
-        axios.post('http://localhost:8081/user/makedonation', {name,email,phone,item,quantity,location})
-        .then(result => console.log(result))
-        .catch(err => console.log(err))
-    }
-    return (
-        <div className='donPage'>
-            <div className='border'>
-                <form onSubmit={Donate}>
-                    <h2>Make Donations</h2>
-                    <div className='mb-3'>
-                        <label htmlFor='name'><strong>Name</strong></label>
-                        <input type='text' placeholder='Enter Name' className='form-control' 
-                        onChange={(e) => setName(e.target.value)}/>
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor='email'><strong>Email</strong></label>
-                        <input type='email' placeholder='Enter Email' className='form-control' 
-                        onChange={(e) => setEmail(e.target.value)}/>
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor='phone'><strong>Phone number</strong></label>
-                        <input type='text' placeholder='Enter Phone' className='form-control' 
-                        onChange={(e) => setPhone(e.target.value)} />
-                    </div>
-                    
-                    <div className='mb-3'>
-                        <label htmlFor='item'><strong>Size Of Item</strong></label>
-                        <input type='text' placeholder='Enter size' className='form-control' 
-                        onChange={(e) => setSize(e.target.value)}/>
-                    </div>
-                    
-                    <div className='mb-3'>
-                        <label htmlFor='quantity'><strong>Quantity</strong></label>
-                        <input type='text' placeholder='Enter Quantity' className='form-control' 
-                        onChange={(e) => setQuantity(e.target.value)}/>
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor='location'><strong>Location</strong></label>
-                        <input type='text' placeholder='Enter Location' className='form-control' 
-                        onChange={(e) => setLocation(e.target.value)}/>
-                    </div>
-                    <Link to="/donhis" className="btn1"> Donate </Link>
-                </form>
-            </div>
-        </div>
-    );
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState({});
+  const [userData, setUserData] = useState(null);
+
+  const handleInput = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post('http://localhost:8081/user/donor/add', values)
+      
+      .then((res) => {
+        if (res.status === 200) {
+          setUserData(res.data);
+          console.log('email')
+          navigate('/donhis', { state: { data: 'email' } });
+          
+        } else {
+          alert('Donation was unsuccessful. Check your credentials.');
+        }
+      })
+      .catch((err) => {
+        console.error('Error while fetching user data:', err);
+        alert('An error occurred while fetching user data.');
+      });
+/*
+     axios
+      .get('http://localhost:8081/user/donor/get', values)
+      .then((res) => {
+        if (res.status === 200) {
+          setUserData(res.data);
+          navigate('/donhis');
+        } else {
+          alert('Donation was unsuccessful. Check your credentials.');
+        }
+      })
+      .catch((err) => {
+        console.error('Error while fetching user data:', err);
+        alert('An error occurred while fetching user data.');
+      });*/
+
+    };
+
+  return (
+    <div className="donPage">
+      <div className="border">
+        <form action="" onSubmit={handleSubmit}>
+          <h2>Make Donations</h2>
+
+          <div className="mb-3">
+            <label htmlFor="name">
+              <strong>Name</strong>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Name"
+              className="form-control"
+              name="name"
+              onChange={handleInput}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              className="form-control"
+              name="email"
+              onChange={handleInput}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="phone">
+              <strong>Phone number</strong>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Phone"
+              className="form-control"
+              name="phone"
+              onChange={handleInput}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="item">
+              <strong>Size Of Item</strong>
+            </label>
+            <input
+              type="text"
+              placeholder="size of item"
+              className="form-control"
+              name="item"
+              onChange={handleInput}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="quantity">
+              <strong>Quantity</strong>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Quantity"
+              className="form-control"
+              name="quantity"
+              onChange={handleInput}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="location">
+              <strong>Location</strong>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter location"
+              className="form-control"
+              name="location"
+              onChange={handleInput}
+            />
+          </div>
+
+          <button type="submit" className="btn">
+            Donate
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Makedon;
