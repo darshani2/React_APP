d5988183e038fa6646662d678712e08bf54cbf90
darshani2 <sewwandidarshani95@gmail.com>
2024-03-17 11:11:37 -0700

    Small New Changes

--- server/models/school.js
+++ server/models/school.js
@@ -1,27 +1,14 @@
 const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
 
 const schoolSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
-  //phone_num : Number,
-  
   verificationToken: String,
   isVerified: Boolean,
   role: String,
-}
-//{timestamps : true}
-);
-/*
-schoolSchema.methods.generateJWT = function(){
-  const token = jwt.sign({
-    _id : this._id,
-    number : this.number
-  }, process.env.JWT_SECRET_KEY, {expireIn : "7d"})
-  return token
-}
-*/
+});
+
 const School = mongoose.model('School', schoolSchema);
 
 module.exports = School;
--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors(
-  {origin: 'http://localhost:3000',methods: ['GET', 'POST'],optionsSuccessStatus: 204,
+  {origin: 'http://localhost:3000',methods: ['GET', 'POST', 'PUT', 'DELETE'],optionsSuccessStatus: 204,
   })
 );
 
