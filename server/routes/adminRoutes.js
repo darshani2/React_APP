013e8923def43a4d8ae2c0a6a44d863a9f8ccdbd
apsara <apziiiii97@gmail.com>
2024-03-18 23:55:29 +0530

    changed routes

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -5,5 +5,6 @@ const adminController = require('../controllers/adminController');
 router.post('/register', adminController.userRegister);
 router.post('/verify', adminController.userVerify);
 router.post('/login', adminController.userLogin);
+router.get("/get", adminController.getAllDonor);
 
 module.exports = router;
