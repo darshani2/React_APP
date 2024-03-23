69ade510cfbcdd9b2f23d760fad4832439b45df9
apsara <apziiiii97@gmail.com>
2024-03-24 01:53:34 +0530

    routes added

--- server/routes/schoolRoutes.js
+++ server/routes/schoolRoutes.js
@@ -5,6 +5,7 @@ const schoolController = require("../controllers/schoolController");
 router.post("/register", schoolController.userRegister);
 router.post("/verify", schoolController.userVerify);
 router.post("/login", schoolController.userLogin);
+router.post("/otp", schoolController.otp);
 router.post("/add", schoolController.addSchool);
 router.get("/", schoolController.getAllSchool);
 router.get("/get", schoolController.getSchoolByEmail);
